344a2d87fe44224334c7120479d01e8c1650505e
Therese Lybo <therese.lybo@gmail.com>
2020-02-17 14:52:00 +0100

    Index
    Finished code on index.js. Needs cleaning up

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const baseUrl = `https://rickandmortyapi.com/api/character/`;
+
+fetch(baseUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        createCharacter(json);
+    })
+    .catch(function() {
+        document.location.href = "error.html";
+        });
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
+
+function createCharacter(json) {
+    const results = json.results;
+    const container = document.querySelector(".results");
+    let html = "";
+
+    results.forEach(function(character) {
+        const name = character.name; 
+        const imageUrl = character.image;
+        let type;
+        if(character.type) {
+            type = character.type;
+        } else {
+            type = "Unknown";
+        }
+        const episodes = character.episode.length;
+
+        const characterDetails = `<div class="col-sm-6 col-md-4 col-lg-3">                
+                                    <div class="card">        
+                                        <img class="image" src="${imageUrl}" alt="${name}">
+                                        <div class="details">
+                                            <h4 class="name">${name}</h4>
+                                            <p>Type: ${type}</p>    
+                                            <p>Episode count: ${episodes}</p>                                  
+                                            <a class="btn btn-primary" href="details.html?id=${character.id}">Details</a>
+                                        </div>
+                                    </div>
+                                </div>`;
+        html += characterDetails;
+    })
+
+    container.innerHTML = html;
+    
+    // const loader = document.querySelector(".loader");
+    // loader.style.display = "none";
+    // container.removeChild(loader);
+}
